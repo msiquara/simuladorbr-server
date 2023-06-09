50d635e2d24e9f34ad2d2e3a1980dea6e8f918c2
msiquara <msiquara@outlook.com>
2023-06-09 17:18:21 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const Pool = require("pg").Pool;
 require('dotenv').config()
 
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+const DB_PORT = process.env.DB_PORT
+
 const pool = new Pool({
-    host: "containers-us-west-146.railway.app",
-    user: "postgres",
-    password: "GfiszJ86CaJkbjoqoCe4",
-    database: "railway",
-    port: 6750,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
 });
 
 module.exports = pool
